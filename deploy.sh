1ebb995d55735dd07d52f3c6530d4e242f9dc569
udryan10 <rgrothouse@gmail.com>
2014-12-18 12:25:48 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker pull udryan10/water-cooler
 docker kill water-cooler
 docker rm water-cooler
-docker run -d --name "water-cooler" -p 8058:80 udryan10/water-cooler
+docker run -d --name "water-cooler" -p 80:80 udryan10/water-cooler
