3e28eb2bdde8f9a77d6e232e32f67d884c1afa71
Allan Clark <allan.clark@ge.com>
2014-12-21 18:14:45 -0500

    added mocha test for session count

---
+++ test/test-server.js
@@ -0,0 +1,27 @@
+var should = require('should');
+var io = require('socket.io-client');
+
+var socketURL = 'http://0.0.0.0';
+
+var options ={
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+describe("water-cooler",function(){
+  // If there are any existing browser sessions open they will break the test
+  it('Should broadcast a new session once they connect',function(done){
+    var client = io.connect(socketURL, options);
+
+    client.on('connect',function(data){
+      client.emit('connection_change', "1");
+    });
+
+    client.on('connection_change',function(sessionCount){
+      sessionCount.should.be.a.Number;
+      sessionCount.should.equal(1);
+      client.disconnect();
+      done();
+    });
+  });
+});
